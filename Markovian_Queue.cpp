240a257b60e07e84578bcc50da4d58cf612d3741
surbhi singh <surbhisingh9592@gmail.com>
2014-10-14 12:03:45 -0500

    Update Markovian_Queue.cpp

--- Markovian_Queue.cpp
+++ Markovian_Queue.cpp
@@ -5,66 +5,66 @@
 // Simulates an General queueing system.  The simulation terminates once 500000 customers depart from the system.
 int main(){
 	using namespace std;
-	enum { ARR, DEP1, DEP2, DEP3 };     // Define the 1 arrival for 3 queues and 3 final departure rates
-	double lambda = 0.0;         // for Arrival rate
-	double mu1 = 0.0;              // Service rate for queue 1
-	double mu2 = 0.0;              // Service rate for queue 2
-	double mu3 = 0.0;              // Service rate for queue 3
-	double rs1 = 0.0;		//probabilty for arriving customer enters queue 1
-	double rs2 = 0.0;		//probabilty for arriving customer enters queue 2
-	double r11 = 0.0;		//probabilty for arriving customer depart queue 1, enters queue 1
-	double r13 = 0.0;		//probabilty for arriving customer depart queue 1, enter queue 3
-	double r3d = 0.0;		//probabilty for arriving customer departs queue 3, leaves network
-	double r32 = 0.0;		//probabilty for arriving customer departs queue 3, ,goes back to queue 2
+	enum { ARR, DEP1, DEP2, DEP3 };     	// Define the 1 arrival for 3 queues and 3 final departure rates
+	double lambda = 0.0;         		// for Arrival rate
+	double mu1 = 0.0;              		// Service rate for queue 1
+	double mu2 = 0.0;              		// Service rate for queue 2
+	double mu3 = 0.0;              		// Service rate for queue 3
+	double rs1 = 0.0;			//probabilty for arriving customer enters queue 1
+	double rs2 = 0.0;			//probabilty for arriving customer enters queue 2
+	double r11 = 0.0;			//probabilty for arriving customer depart queue 1, enters queue 1
+	double r13 = 0.0;			//probabilty for arriving customer depart queue 1, enter queue 3
+	double r3d = 0.0;			//probabilty for arriving customer departs queue 3, leaves network
+	double r32 = 0.0;			//probabilty for arriving customer departs queue 3, ,goes back to queue 2
 
-	double clock = 0.0;           // System clock
-	int Ndep = 0; 			//to generate overall dept
-	double area1 = 0.0;			  //area of the region for queue 1
-	double area2 = 0.0;			  //area of the region for queue 2
-	double area3 = 0.0;			  //area of the region for queue 3
+	double clock = 0.0;           		// System clock
+	int Ndep = 0; 				//to generate overall dept
+	double area1 = 0.0;			//area of the region for queue 1
+	double area2 = 0.0;			//area of the region for queue 2
+	double area3 = 0.0;			//area of the region for queue 3
 
 	double rho = 0.0;
-	double eNSimulation1 = 0.0;//expected number of customer for queue 1
-	double eNSimulation2 = 0.0;//expected number of customer for queue 2
-	double eNSimulation3 = 0.0;//expected number of customer for queue 3
+	double eNSimulation1 = 0.0;		//expected number of customer for queue 1
+	double eNSimulation2 = 0.0;		//expected number of customer for queue 2
+	double eNSimulation3 = 0.0;		//expected number of customer for queue 3
 
-	double eTSimulation1 = 0.0;//expected time for queue 1
-	double eTSimulation2 = 0.0;//expected time for queue 2
-	double eTSimulation3 = 0.0;//expected time for queue 3
-	double throughputSim1 = 0.0;//throughput for queue 1
-	double throughputSim2 = 0.0;//throughput for queue 2
-	double throughputSim3 = 0.0;//throughput for queue 3
+	double eTSimulation1 = 0.0;		//expected time for queue 1
+	double eTSimulation2 = 0.0;		//expected time for queue 2
+	double eTSimulation3 = 0.0;		//expected time for queue 3
+	double throughputSim1 = 0.0;		//throughput for queue 1
+	double throughputSim2 = 0.0;		//throughput for queue 2
+	double throughputSim3 = 0.0;		//throughput for queue 3
 
-	double utilizationSim1 = 0.0;//utilization for queue 1
-	double utilizationSim2 = 0.0;//utilization for queue 2
-	double utilizationSim3 = 0.0;//utilization for queue 3
+	double utilizationSim1 = 0.0;		//utilization for queue 1
+	double utilizationSim2 = 0.0;		//utilization for queue 2
+	double utilizationSim3 = 0.0;		//utilization for queue 3
 
 	//required for theoritical calculation
-	double eNTheoritical1 = 0.0;//expected number of customer for queue 1
-	double eNTheoritical2 = 0.0;//expected number of customer for queue 2
-	double eNTheoritical3 = 0.0;//expected number of customer for queue 3
-	double eTTheoritical1 = 0.0;//expected time for queue 1
-	double eTTheoritical2 = 0.0;//expected time for queue 2
-	double eTTheoritical3 = 0.0;//expected time for queue 3
-	double throughputTheo1 = 0.0;//through put for queue 1
-	double throughputTheo2 = 0.0;//through put for queue 2
-	double throughputTheo3 = 0.0;//through put for queue 3
-	double utilizationTheo1 = 0.0;//utilization for queue 1
-	double utilizationTheo2 = 0.0;//utilization for queue 2
-	double utilizationTheo3 = 0.0;//utilization for queue 3
+	double eNTheoritical1 = 0.0;		//expected number of customer for queue 1
+	double eNTheoritical2 = 0.0;		//expected number of customer for queue 2
+	double eNTheoritical3 = 0.0;		//expected number of customer for queue 3
+	double eTTheoritical1 = 0.0;		//expected time for queue 1
+	double eTTheoritical2 = 0.0;		//expected time for queue 2
+	double eTTheoritical3 = 0.0;		//expected time for queue 3
+	double throughputTheo1 = 0.0;		//through put for queue 1
+	double throughputTheo2 = 0.0;		//through put for queue 2
+	double throughputTheo3 = 0.0;		//through put for queue 3
+	double utilizationTheo1 = 0.0;		//utilization for queue 1
+	double utilizationTheo2 = 0.0;		//utilization for queue 2
+	double utilizationTheo3 = 0.0;		//utilization for queue 3
 
-	EventList Elist;              // Create event list
+	EventList Elist;              		// Create event list
 
-	int currentCustCount1; 	//current customer in queue 1
-	int currentCustCount2; 	//current customer in queue 2
-	int currentCustCount3;  //current customer in queue 3
-	int totalArr1;  	//total number of arrivals in queue 1
-	int totalArr2		 //total number of arrivals in queue 2
-	int totalArr3;  	//total number of arrivals in queue 3
-	int totalDept1;  	//total number of departures in queue 1
-	int totalDept2;  	//total number of departures in queue 2
-	int totalDept3;  	//total number of departures in queue 3
-	int done;  		//total number of departure for all queues
+	int currentCustCount1; 			//current customer in queue 1
+	int currentCustCount2; 			//current customer in queue 2
+	int currentCustCount3;  		//current customer in queue 3
+	int totalArr1;  			//total number of arrivals in queue 1
+	int totalArr2		 		//total number of arrivals in queue 2
+	int totalArr3;  			//total number of arrivals in queue 3
+	int totalDept1;  			//total number of departures in queue 1
+	int totalDept2;  			//total number of departures in queue 2
+	int totalDept3;  			//total number of departures in queue 3
+	int done;  				//total number of departure for all queues
 
 
 
